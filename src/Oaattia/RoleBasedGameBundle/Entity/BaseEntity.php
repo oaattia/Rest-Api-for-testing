ae16998d04d625e92e16d054ffc02c010408aef5
Osama Ahmed <oaattia@gmail.com>
2017-06-13 20:50:33 +0000

    finish the entity

---
+++ src/Oaattia/RoleBasedGameBundle/Entity/BaseEntity.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Oaattia\RoleBasedGameBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\MappedSuperclass()
+ *
+ * Class BaseEntity
+ * @package Oaattia\RoleBasedGameBundle\Entity
+ */
+class BaseEntity
+{
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\PrePersist()
+     */
+    public function prePresist()
+    {
+        $this->createdAt = new \DateTime("now");
+        $this->updatedAt = new \DateTime("now");
+    }
+
+    /**
+     * @ORM\PreUpdate()
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime("now");
+    }
+}
